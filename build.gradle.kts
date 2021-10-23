261819d7ee10d4e0e98b40a4adceb15f0143fb41
Sambo Chea <sombochea@cubetiqs.com>
2021-10-23 11:21:43 +0700

    Update 'build.gradle.kts'

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.5.5" apply false
+    id("org.springframework.boot") version "2.5.6" apply false
     id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
     kotlin("jvm") version "1.5.31" apply false
     kotlin("plugin.spring") version "1.5.31" apply false
